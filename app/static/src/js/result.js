741f859b792511cfa981cd427a03a4a434042012
YuCheng <fyc621@gmail.com>
2021-09-19 21:58:18 +0800

    adjust: use page css color variable on chartjs color setting #20210919004

--- app/static/src/js/result.js
+++ app/static/src/js/result.js
@@ -4,101 +4,138 @@ $(document).ready(function () {
 })
 
 /**
+ * Theme Color
+ */
+const style = getComputedStyle(document.body);
+const theme = {};
+theme.primary = style.getPropertyValue('--bs-primary');
+theme.secondary = style.getPropertyValue('--bs-secondary');
+theme.success = style.getPropertyValue('--bs-success');
+theme.info = style.getPropertyValue('--bs-info');
+theme.warning = style.getPropertyValue('--bs-warning');
+theme.danger = style.getPropertyValue('--bs-danger');
+theme.light = style.getPropertyValue('--bs-light');
+theme.dark = style.getPropertyValue('--bs-dark');
+
+/**
  * chartJs
  */
-const radarData = {
-    labels: [
-        // [radar[3][0].substr(0,4), radar[3][0].substr(4)],
-        radar[3][0],
-        // [radar[1][0].substr(0,3), radar[1][0].substr(3)],
-        radar[1][0].split(''),
-        // [radar[0][0].substr(0,4), radar[0][0].substr(4)],
-        radar[0][0],
-        // [radar[2][0].substr(0,5), radar[2][0].substr(5)],
-        radar[2][0].split('')
-    ],
-    datasets: [{
-        data: [radar[3][3], radar[1][3], radar[0][3], radar[2][3]],
+
+// 資料標籤
+const labels = [
+    // [radar[3][0].substr(0,4), radar[3][0].substr(4)],
+    radar[3][0],
+    // [radar[1][0].substr(0,3), radar[1][0].substr(3)],
+    radar[1][0].split(''),
+    // [radar[0][0].substr(0,4), radar[0][0].substr(4)],
+    radar[0][0],
+    // [radar[2][0].substr(0,5), radar[2][0].substr(5)],
+    radar[2][0].split('')
+]
+
+// 資料值
+const data = [
+    radar[3][3],
+    radar[1][3],
+    radar[0][3],
+    radar[2][3]
+]
+
+// 資料集設定
+const dataset = [
+    {
+        data: data,
         fill: true,
-        backgroundColor: 'rgba(76,175,80, 0.2)',
-        borderColor: 'rgb(76,175,80)',
-        pointBackgroundColor: 'rgba(76,175,80,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: 'rgba(255,255,255,1)',
-        pointHoverBorderColor: 'rgb(76,175,80)',
+        backgroundColor: `${theme.primary}20`,
+        borderColor: `${theme.primary}`,
+        pointBackgroundColor: `${theme.primary}`,
+        pointBorderColor: `${theme.light}`,
+        pointHoverBackgroundColor: `${theme.warning}`,
+        pointHoverBorderColor: `${theme.primary}`,
         pointBorderWidth: 1,
         pointRadius: 7,
         pointHoverRadius: 10,
         pointStyle: 'circle',
         hidden: false
-    }]
+    }
+]
+
+// 雷達圖資料設定
+const radarData = {
+    labels: labels,
+    datasets: dataset
 };
 
-const radarConfig = {
-    type: 'radar',
-    data: radarData,
-    options: {
-        elements: {
-            line: {
-                borderWidth: 3
+// 雷達圖屬性設定
+const radarOptions = {
+    elements: {
+        line: {
+            borderWidth: 3
+        }
+    },
+    // 額外項目
+    plugins: {
+        // 圖例
+        legend: {
+            // 顯示標籤
+            display: false,
+            labels: {
+                color: `${theme.primary}`
             }
         },
-        // 額外項目
-        plugins: {
-            // 圖例
-            legend: {
-                // 顯示標籤
-                display: false,
-                labels: {
-                    color: 'rgba(29, 66, 30, 1)'
+        tooltip: {
+            enabled: true,
+            backgroundColor: `${theme.warning}50`,
+            titleFont: {
+                size: 20
+            },
+            titleColor: `${theme.dark}`,
+            bodyFont: {
+                size: 20
+            },
+            bodyColor: `${theme.dark}`,
+            usePointStyle: true,
+            displayColors: true
+        },
+    },
+    scales: {
+        r: {
+            beginAtZero: true,
+            min: 0,
+            max: 100,
+            ticks: {
+                font: {
+                    size: 20
+                },
+                color: `${theme.dark}`,
+                maxTicksLimit: 10,
+            },
+            pointLabels: {
+                color: `${theme.dark}`,
+                font: {
+                    size: 20,
+                    weight: 700
                 }
             },
-            tooltip: {
-                enabled: true,
-                backgroundColor: 'rgba(161,192,224,0.5)',
-                titleFont: {
-                    size: 20
-                },
-                titleColor: 'black',
-                bodyFont: {
-                    size: 20
-                },
-                bodyColor: 'black',
-                usePointStyle: true,
-                displayColors: true
+            angleLines: {
+                color: `${theme.primary}`
             },
-        },
-        scales: {
-            r: {
-                beginAtZero: true,
-                min: 0,
-                max: 100,
-                ticks: {
-                    font: {
-                        size: 20
-                    },
-                    color: 'rgba(29, 66, 30, 1)',
-                    maxTicksLimit: 10,
-                },
-                pointLabels: {
-                    color: 'rgba(29, 66, 30, 1)',
-                    font: {
-                        size: 20,
-                        weight: 700
-                    }
-                },
-                angleLines: {
-                    color: 'rgba(29, 66, 30, 0.5)'
-                },
-                grid: {
-                    color: 'rgba(29, 66, 30, 1)'
-                },
-            }
+            grid: {
+                color: `${theme.primary}`
+            },
         }
     }
+}
+
+// 圖表總資訊
+const chartInfo = {
+    type: 'radar',
+    data: radarData,
+    options: radarOptions
 };
 
-var myRadar = new Chart(
+// 建立圖表
+const myRadar = new Chart(
     document.getElementById('myRadar'),
-    radarConfig
+    chartInfo
 );
