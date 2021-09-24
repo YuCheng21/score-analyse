3c65caf4095219383471df5fbd8afa7958ee06c0
YuCheng <fyc621@gmail.com>
2021-09-24 11:31:59 +0800

    adjust: use current_app instead context_processor to apply jinja function #20210924002

--- app/view/root/__init__.py
+++ app/view/root/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for, session, abort
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session, abort, current_app
 from flask.views import MethodView
 
 from ...model.transcript import TranscriptModel
@@ -8,15 +8,6 @@ from ...model.analyse import AnalyseModel, skill_tree_label
 from ...model.jinja import col_len, is_pass
 
 app = Blueprint('root', __name__)
-
-
-@app.context_processor
-def utility_processor():
-    utilities = {
-        'col_len': col_len,
-        'is_pass': is_pass
-    }
-    return utilities
 
 
 @app.route('/index', methods=['GET', 'POST'])
@@ -33,7 +24,10 @@ def index():
 class Result(MethodView):
     def get(self):
         title = '分析結果'
-
+        # custom template function
+        current_app.jinja_env.globals['is_pass'] = is_pass
+        current_app.jinja_env.globals['col_len'] = col_len
+        # get results session
         student_id = session.get('student_id')
         student_name = session.get('student_name')
         score_list = session.get('score_list')
@@ -45,15 +39,16 @@ class Result(MethodView):
         return render_template('./root/result.html', **locals())
 
     def post(self):
-        # Get PDF
+        # Get PDF file
         singleCertInput = request.files['singleCertInput']
         try:
             # Extract the PDF into a dictionary from the keys student_id, student_name and score_list
             pdf_info = TranscriptModel().extract(singleCertInput)
         except:
+            # check file is right or not
             flash('檔案內容錯誤！請確認上傳校務系統下載的成績單檔案。', category='error')
             return redirect(url_for('root.index'))
-
+        # processing and analyse pdf
         student_id, student_name, score_list = pdf_info
         buffer = PassedCourse().get_target_table(student_id)
         score_list += list(i for i in buffer)
@@ -63,7 +58,7 @@ class Result(MethodView):
         result_info = AnalyseModel().progress_calc(score_list)
         passed_skill_tree, radar_array = result_info
         passed_type = list(i[0] for i in radar_array if i[3] >= 100)
-
+        # save results in session
         session['student_id'] = student_id
         session['student_name'] = student_name
         session['score_list'] = score_list
