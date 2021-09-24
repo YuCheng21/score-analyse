41c2b9c84e5a4dcaca70da58f911b33903aef589
YuCheng <fyc621@gmail.com>
2021-09-24 11:42:18 +0800

    adjust: use global variable for website name #20210924003

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 import os
 
 from .config.flask import config as flask_config
 from .config.flask import logger as handler
-from .config.base import project_path
+from .config.base import project_path, website_name
 
 from .view.root import app as root
 
@@ -17,6 +17,10 @@ def create_app(config_name):
     app.register_blueprint(root)
 
     app.logger.addHandler(handler)
+
+    @app.before_request
+    def before_request():
+        g.website_name = website_name
 
     @app.errorhandler(404)
     def page_not_found(e):
--- app/config/base.py
+++ app/config/base.py
@@ -2,3 +2,5 @@ import os
 
 project_path = os.path.abspath('./')
 csv_path = os.path.join(project_path, 'app', 'passed.csv')
+
+website_name = '學程進度查詢'
